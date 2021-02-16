d8e30f7886225c8a25364695200f93f018fa8d56
Lucy Gu <wg74@duke.edu>
2021-02-15 20:20:10 -0800

    attempt

--- reliable_transport/ReliableImpl.py
+++ reliable_transport/ReliableImpl.py
@@ -21,7 +21,39 @@ class ReliableImpl:
     @staticmethod
     def checksum(s):
         # TODO: Your code here
-        return 0
+        #s = '\x00\x01\xf2\x03\xf4\xf5\xf6\xf7'
+        s1 = 0
+        s2 = 0
+        for i in range(len(s)):
+            if(i%2==0):
+                s1+=s[i]
+            else:
+                s2+=s[i]
+        a = True
+        while a:
+            x1 = 0
+            x2 = 0
+            if s1 < 256:
+                if s2<256:
+                    a = False
+            if s1>=256:
+                x1 = s1//256
+                s1 = s1%256
+            if s2>=256:
+                x2 = s2//256
+                s2 = s2%256
+            s1 = s1+x2
+            s2 = s2+x1
+            if s1 < 256:
+                if s2<256:
+                    a = False
+        ret = s1*256 + s2
+        print(hex(ret))
+        return ret
+
+
+
+
 
     # recvAck: When an ACK or FINACK segment is received, the framework will
     # call this function to handle the segment.
@@ -35,6 +67,8 @@ class ReliableImpl:
     def recvAck(self, seg, isFin):
         # TODO: Your code here
         return 0
+# scp -oProxyCommand="ssh -W %h:%p wg74@vcm-18887.vm.duke.edu"  /home/vcm/compsci_ece356_spring2021_labs/reliable_transport/Receiver  wg74@cs356.cs.duke.edu:~/
+# ssh -oProxyCommand="ssh -W %h:%p wg74@vcm-18887.vm.duke.edu"  wg74@cs356.cs.duke.edu "pkill -u wg74 Receiver; ./Receiver temp.txt -p 7017 -s 50 -f 25"
 
     # sendData: This function is called when a block of data should be sent out.
     # You can call Segment.pack in Util.py to encapsulate a segment and
@@ -54,3 +88,7 @@ class ReliableImpl:
     def retransmission(self, seqNum):
         # TODO: Your code here
         pass
+
+
+# scp -oProxyCommand="ssh -W %h:%p wg74@vcm-18887.vm.duke.edu" /home/vcm/compsci_ece356_spring2021_labs/reliable_transport/trace1.pcap ~/Documents/ECE356
+
