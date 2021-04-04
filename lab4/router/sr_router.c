fd9846b6442c9455b89ca7547a8e92cbf3f5bcb1
Lucy Gu <wg74@duke.edu>
2021-04-04 03:19:01 -0700

    ;

--- lab4/router/sr_router.c
+++ lab4/router/sr_router.c
@@ -175,7 +175,7 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t * buf, unsigned int len,char* 
         uint8_t * block = malloc(ntohs(ip->ip_len) + sizeof(sr_ethernet_hdr_t));
         sr_ip_hdr_t* pkt = (sr_ip_hdr_t *)(block + sizeof(sr_ethernet_hdr_t));
         memcpy(pkt, ip, ntohs(ip->ip_len));
-        struct sr_if *iface = sr_get_interface(sr, interface);
+        struct sr_if *iface = sr_get_interface(sr, match->interface);
         sr_ethernet_hdr_t* start_of_pckt = (sr_ethernet_hdr_t*) block;
         /*save sr_arpcache_ lookup as struct sr_arp_entry, put that in if(sr_arp = true), */
         struct sr_arpentry * entry = sr_arpcache_lookup( &(sr->cache), ip->ip_dst);
