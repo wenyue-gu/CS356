03ced4b01b81d38db0fa59206f27fa3009919112
Lucy Gu <wg74@duke.edu>
2021-04-04 03:24:37 -0700

    ;

--- lab4/router/sr_router.c
+++ lab4/router/sr_router.c
@@ -184,7 +184,7 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t * buf, unsigned int len,char* 
           printf("2c3 entry not null\n");
           memcpy((void *) (start_of_pckt->ether_shost), srcMac->addr, sizeof(uint8_t) * ETHER_ADDR_LEN);
           memcpy((void *) (start_of_pckt->ether_dhost), entry->mac, sizeof(uint8_t) * ETHER_ADDR_LEN);
-          start_of_pckt->ether_type = ethertype_ip;
+          start_of_pckt->ether_type = htons(ethertype_ip);
           print_hdrs(block,ntohs(ip->ip_len) + sizeof(sr_ethernet_hdr_t));
           sr_send_packet(sr, block, ntohs(ip->ip_len) + sizeof(sr_ethernet_hdr_t), interface);
         }
