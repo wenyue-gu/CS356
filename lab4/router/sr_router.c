103ed85e36ca3907c165d433843ca0f6ef2ff3cc
Lucy Gu <wg74@duke.edu>
2021-04-21 11:53:57 -0700

    .

--- lab4/router/sr_router.c
+++ lab4/router/sr_router.c
@@ -233,6 +233,10 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t * buf, unsigned int len,char* 
             struct sr_arpentry * entry;
             /*LAB5 1b2*/
             printf("match gwsaddr is %s\n",inet_ntoa(match->gw));
+
+            memcpy((void *) (start_of_pckt->ether_shost), iface2->addr, sizeof(uint8_t) * ETHER_ADDR_LEN);
+            start_of_pckt->ether_type = htons(ethertype_ip);
+
             if(match->gw.s_addr != 0){
               entry = sr_arpcache_lookup(&(sr->cache), match->gw.s_addr);
             }
@@ -241,7 +245,6 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t * buf, unsigned int len,char* 
             }
             if(entry!=NULL){
               printf("2c3 entry not null\n");
-              memcpy((void *) (start_of_pckt->ether_shost), iface2->addr, sizeof(uint8_t) * ETHER_ADDR_LEN);
               /*LAB5 2*/
               unsigned char value[ETHER_ADDR_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
               if(entry->mac==value){ 
@@ -254,7 +257,7 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t * buf, unsigned int len,char* 
               uint32_t target = pkt->ip_dst;
               pkt->ip_dst = source;
               pkt->ip_src = target;*/
-              start_of_pckt->ether_type = htons(ethertype_ip);
+              
               print_hdrs(block,ntohs(ip->ip_len) + sizeof(sr_ethernet_hdr_t));
               sr_send_packet(sr, block, ntohs(ip->ip_len) + sizeof(sr_ethernet_hdr_t), match->interface);
               free(block);
@@ -268,9 +271,10 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t * buf, unsigned int len,char* 
               send modified packet immediately
             }*/
             else /*(did not contain dest IP)*/ {
-              printf("2c3 else did not contain dest ip\n");
+              printf("2c3 else did not contain dest ip entry is null\n");
               sr_arpcache_queuereq(&sr->cache, ip->ip_dst, (uint8_t *) block, ntohs(ip->ip_len) + sizeof(sr_ethernet_hdr_t), interface);
-              printf("sending arp req\n");
+              /*printf("sending arp req\n");*/
+              print_hdrs(block,ntohs(ip->ip_len) + sizeof(sr_ethernet_hdr_t));
               send_arp_req(sr, iface2, ip->ip_dst, sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t));
               free(block);
               /*dont send modified pack
