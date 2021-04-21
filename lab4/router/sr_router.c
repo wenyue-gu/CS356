cacaca72b88efbbaf8ada3907b597293adb6f643
Lucy Gu <wg74@duke.edu>
2021-04-21 11:42:34 -0700

    .

--- lab4/router/sr_router.c
+++ lab4/router/sr_router.c
@@ -202,11 +202,12 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t * buf, unsigned int len,char* 
         /*If you can not find this destination IP in your routing table, 
         you should send an ICMP DEST_NET_UNREACHABLE message back to the Sender. 
         You should implement a Longest Prefix Matching here.*/
-        struct in_addr ipdst;
-        ipdst.s_addr = ip->ip_dst;
         struct sr_rt * match = prefix_match(sr,ip->ip_dst);
+
+        /*struct in_addr ipdst;
+        ipdst.s_addr = ip->ip_dst;
         printf("ip destination %s\n", inet_ntoa(ipdst));
-        sr_print_routing_entry(match);
+        sr_print_routing_entry(match);*/
         if(match==NULL){
           
           icmp_unreachable(sr, Unreachable_net_code, ip, interface);
